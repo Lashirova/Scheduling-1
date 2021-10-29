59a0fd92e48638a6f6c4fddc86569e6e1396a9c5
Lachin Ashirova <60476768+Lashirova@users.noreply.github.com>
2021-10-29 00:42:14 +0000

    removed unnecessary explanations

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #Implementing Priority Queues with Sorted list
-#declaring empty list
-que=[] #adding elements to the list
+que=[] 
 que.append((1,064.0,'Medium priority task'))
 que.append((1,198.4,'High priority task'))
 que.append((141.4,'Low priority task'))
-#list is sorted evertime a new element is inserted
 que.sort(reverse=True)
 print("Tasks with their priorities :")
-#looping through sorted list
 while que:
       queue_item=que.pop()
       print(queue_item)
+
+
+
 
 # Implementing priority queue using queue.PriorityQueue class
 import queue as PQ
